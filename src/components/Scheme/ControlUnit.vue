19d82ed0bfaa11150b032bdf3ef92453935a5189
Rrewer2 <rrewer2018@gmail.com>
2024-11-25 07:32:08 +0100

    added P and T pipes

--- src/components/Scheme/ControlUnit.vue
+++ src/components/Scheme/ControlUnit.vue
@@ -12,9 +12,11 @@ const s = () => x + 3 * R - (R / (6 - length));
     <DR2 :x="x" :y="y + 300 + sl / 10" :type="unit.DR2type" :N="unit.HKSH.length" :sl="sl" :sl1="sl1" :sh="sh" />
     <Valve v-for="el, i in unit.HKSH" :x="x + sl1 + i * (sl * 1.1)" :y="y + 300 + sh + sl / 8" :data="el" :sl="sl" />
     <path
-        :d="`M${x + sl / 10} ${y + 300 + sl / 10}  h${-sh / 5} v${sh * 3} h${sh / 5} h${250 - sl / 10 + s()} v${sh * 3}`"
-        stroke="black" stroke-width="3" fill="none" />
-
+        :d="`M${x + sl / 10} ${y + 300 + sl / 10}  h${-sh / 5} v${sh * 3} h${sh / 5} h${250 - sl / 10 + s()} v${sh * 4}`"
+        stroke="red" stroke-width="3" fill="none" />
+    <path
+        :d="`M${x + sl1 + (sl + sl / 10) * unit.HKSH.length} ${y + sl / 10 + 300 + sh} h${sh / 5} v${sh * 2} h${900 - (x + sl1 + (sl + sl / 10) * unit.HKSH.length + sh / 5)} v${sh * 4}`"
+        stroke="blue" stroke-width="2" fill="none" />
 </template>
 
 <style scoped></style>
--- src/components/Scheme/Pump.vue
+++ src/components/Scheme/Pump.vue
@@ -3,6 +3,7 @@ const { x, y, R, data, ver } = defineProps(['x', 'y', 'R', 'data', 'ver']);
 </script>
 
 <template>
+    <path v-if="ver" :d="`M${x} ${y} h${-2 * R} h${3 * R} h${0}`" stroke="black" stroke-width="2" />
     <circle :cx="x" :cy="y" :r="R" stroke="black" stroke-width="2px" fill="white" />
     <path v-if="ver" :d="`M${x + 0.9 * R} ${y} l${-R / 3} ${-R / 5} v${R / 2.5}z`" stroke="black" stroke-width="2" />
     <path v-else :d="`M${x} ${y - 0.9 * R} l${R / 5} ${R / 3} h${-R / 2.5}z`" stroke="black" stroke-width="2" />
