2cb04186a7e6f192059e74059e5c3a7a40f02837
Rrewer2 <rrewer2018@gmail.com>
2025-04-10 07:12:00 +0200

    added list of valves

--- src/components/Oferta.vue
+++ src/components/Oferta.vue
@@ -5,33 +5,35 @@ const { order } = defineProps(["order"]);
 </script>
 
 <template>
-  <table>
-    <thead>
-      <td v-for="a in ['Nr', 'Title', 'Opis', 'Count']">
-        <b><i>{{ a }}</i></b>
-      </td>
-    </thead>
+  <article class="">
+    <table>
+      <thead>
+        <td v-for="a in ['Nr', 'Title', 'Opis', 'Count']">
+          <b><i>{{ a }}</i></b>
+        </td>
+      </thead>
 
-    <tbody>
-      <tr>
-        <td>100</td>
-        <td class="tal">KIT</td>
-        <td>{{ KITtitle(order) }}</td>
-      </tr>
-      <tr v-for="{ title } of order">
-        <td>{{ 200 }}</td>
-        <td class="tal">
-          <!-- <input type="radio" :id="title" v-model="order[`motor${i}`]" :value="({ title, motorData: elem })"
+      <tbody>
+        <tr>
+          <td>100</td>
+          <td class="tal">KIT</td>
+          <td>{{ KITtitle(order) }}</td>
+        </tr>
+        <tr v-for="{ title, ...rest } of order">
+          <td>{{ 200 }}</td>
+          <td class="tal">
+            <!-- <input type="radio" :id="title" v-model="order[`motor${i}`]" :value="({ title, motorData: elem })"
             name="title" :checked="getTitle() === title" class="mx" /> -->
 
-          {{ title }}
+            {{ title }}
 
-        </td>
-        <!-- <td v-for="item in elem">{{ item }}</td> -->
-      </tr>
-    </tbody>
-    {{ order }}
-  </table>
+          </td>
+          <td v-for="item in rest">{{ item }}</td>
+        </tr>
+      </tbody>
+      order {{ order }}
+    </table>
+  </article>
 </template>
 
 <style scoped></style>
