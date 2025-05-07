1e5a750a56bd0b4fae612bdaa3fdd00e5f6b0931
Rrewer2 <rrewer2018@gmail.com>
2025-05-07 22:56:28 +0200

    rewrited Simile with div

--- src/components/Simile.vue
+++ src/components/Simile.vue
@@ -1,59 +1,129 @@
 <script setup>
 const { simile } = defineProps(["simile"]);
-const getDiffBetween = (str1, str2) => {
-  if (!str1.trim().length || !str2.trim().length) return '';
-  const a1 = str1.split("\n").map(row => row.split('\t'));
-  const a2 = str2.split("\n").map(row => row.split('\t'));
-  const obj1 = {};
-  a1.forEach(([nr, title, N]) => {
-    obj1[title] ? obj1[title].push({ N, nr }) : obj1[title] = [{ N, nr }];
-  });
-  const obj2 = {};
-  a2.forEach(([title, N]) => {
-    obj2[title] ? obj2[title].push(N) : obj2[title] = [N];
-  });
-  const res = [];
-  Object.keys(obj1).forEach(key => {
-    const z1 = obj1[key].map(({ N }) => N);
-    const z3 = obj1[key].map(({ nr }) => nr);
-    if (!obj2[key]) {
-      res.push([`${z3.join(', ')}`, key, `${z1.join(', ')}`].join('\t'));
-    }
-    else {
-      const z2 = obj2[key];
-      if (z1.reduce((a, b) => +a + +b) !== z2.reduce((a, b) => +a + +b)) {
-        res.push([`${z3.join(', ')}`, key, `${z1.join(', ')}`, `/ ${z2.join(', ')}`].join('\t'));
-      }
-    }
-  });
-  res.push('	');
-  Object.keys(obj2).forEach(key => {
-    if (!obj1[key]) {
-      res.push([key, `${obj2[key].join(', ')}`].join('\t'));
-    }
-  });
-  return res.join('\n');
+const getDiffBetween = (orderStr, modelStr) => {
+ if (!orderStr.trim().length || !modelStr.trim().length) return '';
+ const order = orderStr.split("\n").map(row => row.split('\t'));
+ const model = modelStr.split("\n").map(row => row.split('\t'));
+ const orderObj = {};
+ let KIT = 0;
+ order.forEach(([nr, title, N]) => {
+ if (!title) return;
+ if (title.match(/^KIT/i)) return KIT = N;
+ orderObj[title] ? orderObj[title].push({ N: +N, nr }) : orderObj[title] = [{ N: +N, nr }];
+ });
+ const modelObj = {};
+ model.forEach(([title, N]) => {
+ if (!title) return;
+ if (title.match(/^M\d+[A-Za-z]*/)) return;
+ if (title.match(/^rura|wąż/gi)) return;
+ const trimTitle = title.trim();
+ modelObj[trimTitle] ? modelObj[trimTitle].push(N * (KIT || 1)) : modelObj[trimTitle] = [N * (KIT || 1)];
+ }
+ );
+ // const res = [];
+ // Object.keys(orderObj).forEach(title => {
+ // const orderCount = orderObj[title].map(({ N }) => N);
+ // const orderNumber = orderObj[title].map(({ nr }) => nr);
+ // if (!modelObj[title]) {
+ // res.push([`${orderNumber.join(', ')}`, title, `${orderCount.join(', ')}`].join('\t'));
+ // }
+ // else {
+ // const modelCount = modelObj[title];
+ // if (orderCount.reduce((a, b) => +a + +b) !== modelCount.reduce((a, b) => +a + +b)) {
+ // res.push([`${orderNumber.join(', ')}`, title, `${orderCount.join(', ')}`, `/ ${modelCount.join(', ')}`].join('\t'));
+ // }
+ // }
+ // });
+ // res.push('	');
+ // Object.keys(modelObj).forEach(title => {
+ // if (!orderObj[title]) {
+ // res.push([title, `${modelObj[title].join(', ')}`].join('\t'));
+ // }
+ // });
+ // return res.join('\n');
+ const res = [];
+ const orderFixed = Object.keys(orderObj).forEach(title => {
+ const orderCount = orderObj[title].map(({ N }) => N);
+ const orderNumber = orderObj[title].map(({ nr }) => nr);
+ if (!modelObj[title]) {
+ res.push([orderNumber, title, orderCount]);
+ }
+ else {
+ const modelCount = modelObj[title];
+ if (orderCount.reduce((a, b) => +a + +b) !== modelCount.reduce((a, b) => +a + +b)) {
+ res.push([orderNumber, title, orderCount, modelCount]);
+ }
+ }
+ });
+ const res2 = [];
+ const modelFixed = Object.keys(modelObj).forEach(title => {
+ if (!orderObj[title]) {
+ res2.push([title, modelObj[title]]);
+ }
+ });
+ return [res, res2];
 };
 </script>
 
 <template>
   <section>
     <textarea name="zlec" id="zlec" placeholder="Wstaw ze zlecenia:
-100 HKHQ012  1" v-model="simile.zlec"></textarea>
+100 HKHQ012 1" v-model="simile.zlec"></textarea>
     <textarea name="lista" id="lista" placeholder="Wklej z listy cześci:
 HKHQ012 2" v-model="simile.lista"></textarea>
   </section>
-  <textarea id="result"
-    placeholder="Różnica między zleceniem a listą części">{{ getDiffBetween(simile.zlec, simile.lista) }}</textarea>
+  <article id="result" class="flex flex-row flex-between container">
+    <h1 v-if="!simile.zlec || !simile.lista">Różnica między zleceniem a listą części</h1>
+    <div v-for="res, i in getDiffBetween(simile.zlec, simile.lista)">
+      <table v-if="!i" v-for="item in res" style="width: 100%; table-layout: fixed;">
+        <tbody>
+          <tr>
+            <td class="text-left" style="width: 20%;">
+              {{ Array.isArray(item[0]) ? item[0].join(' , ') : item[0] }}
+            </td>
+            <td class="text-left" style="width: 40%;">
+              {{ item[1] }}
+            </td>
+            <td style="width: 20%; color: red; -color: black" v-if="item[2]">
+              {{ item[2].join(' , ') }}
+            </td>
+            <td style="width: 20%; color: green">
+              {{ item[3] ? item[3].join(' , ') : '' }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="table" v-else v-for="item in res" style="width: 100%; table-layout: fixed;">
+        <tbody>
+          <tr>
+            <td class="text-left" style="width: 60%;">{{ Array.isArray(item[0]) ? item[0].join(', ') : item[0] }}</td>
+            <td style="width: 40%; color: green">{{ item[1].join(' , ') }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </article>
 </template>
 
 <style scoped>
 textarea {
   text-align: left;
   font-size: xx-large;
-  height: 50vh;
+  height: 40vh;
   width: 40%;
   margin: 20px 10px;
   padding: 10px;
+}
+
+.container {
+  padding: 0 20vh;
+}
+
+.table {
+  margin-left: 55px;
+}
+
+table {
+  width: 40%;
 }
 </style>
