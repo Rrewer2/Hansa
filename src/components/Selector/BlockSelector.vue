641611477338a0bf761367b4daeabbb09effb420
Rrewer2 <115699656+Rrewer2@users.noreply.github.com>
2025-09-09 12:23:29 +0200

    Update BlockSelector.vue

--- src/components/Selector/BlockSelector.vue
+++ src/components/Selector/BlockSelector.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { ref } from "vue";
 import { blockData } from "../../services/data";
 import { getTextWithSpace } from "../../services/functions";
 import { links } from "../../services/links";
 import { text } from "../../services/text";
 import CopyText from "./CopyText.vue";
+import InputItem from "../InputItem.vue";
 
 const { project, meta, order, powerUNIT, i } = defineProps([
   "project",
@@ -12,60 +14,8 @@ const { project, meta, order, powerUNIT, i } = defineProps([
   "powerUNIT",
   "i",
 ]);
-
-const filteredBlock = () => {
-    const data =
-      order[`motor${i}`]?.motorData?.mount.at(-1) === "4"
-        ? couplingsKitData
-        : couplingData;
-    const filtered = data.filter(({ group, size, shaft, pump }) => {
-      return (
-        size === order[`motor${i}`]?.motorData?.size &&
-        (!shaft || shaft === order[`pump${i}`]?.pumpData?.shaft) &&
-        (!group || group === order[`pump${i}`]?.pumpData?.group) &&
-        (!pump || order[`pump${i}`].title?.startsWith(pump))
-      );
-    });
-    if (filtered.length === 1) {
-      const { title, ...rest } = filtered[0];
-      order.coupling = { title, couplingData: { ...rest } };
-    }
-    return filtered;
-};
+console.log(powerUNIT[0].unit);
 </script>
 
-<template>
-  <article>
-    <!-- <h2>
-      {{ text("coupling") }} {{ i ? i + 1 : ""
-      }}<span> {{ filteredCoupling().at(-1)?.title }}</span>
-    </h2> -->
-
-    <br />
-
-    <!-- <table v-if="filteredCoupling().length">
-      <thead>
-        <tr>
-          <td v-for="a in Object.keys(filteredCoupling()[0])">
-            <b><i>{{ a }}</i></b>
-          </td>
-        </tr>
-      </thead>
-      <tbody v-for="{ title, ...rest } in filteredCoupling()">
-        <tr>
-          <td class="tal">
-            <input type="checkbox" :id="title" @click="order.coupling = { title, couplingData: { ...rest } }"
-              class="mx" />
-            <a :href="`${links[meta.lang]}${title}`" target="_blank" rel="noopener noreferrer">
-              {{ getTextWithSpace(title) }}
-            </a>
-            <CopyText :text="title" />
-          </td>
-          <td v-for="item in Object.values(rest)">{{ item }}</td>
-        </tr>
-      </tbody>
-    </table> -->
-  </article>
-</template>
 
 <style scoped></style>
