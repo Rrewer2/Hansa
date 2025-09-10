7d6dca5d610cf69afdba41c7456434859b83acba
Rrewer2 <115699656+Rrewer2@users.noreply.github.com>
2025-09-10 15:27:33 +0200

    Update BlockSelector.vue

--- src/components/Selector/BlockSelector.vue
+++ src/components/Selector/BlockSelector.vue
@@ -1,4 +1,37 @@
 <script setup>
 import { ref } from "vue";
 import { blockData } from "../../services/data";
+import { getTextWithSpace } from "../../services/functions";
+import { links } from "../../services/links";
+import { text } from "../../services/text";
+import CopyText from "./CopyText.vue";
+import InputItem from "../InputItem.vue";
 
+const { project, meta, order, powerUNIT, i } = defineProps([
+  "project",
+  "meta",
+  "order",
+  "powerUNIT",
+  "i",
+]);
+  console.log('Fuck');
+console.log(powerUNIT);
+const filteredBlocks = () => {
+  return blockData.filter((item) => {
+    console.log(item.stations, powerUNIT.unit.length);
+    console.log(powerUNIT.unit.Q);
+    console.log(item.pressure, powerUNIT.unit.p);
+    return (
+      item.stations === powerUNIT.unit.length &&
+      (item.cetop === 3 && powerUNIT.unit.Q < 40 || item.cetop === 5 && powerUNIT.unit.Q > 40)
+      item.pressure > powerUNIT.unit.p + 20
+    );
+  });
+};
+</script>
+
+<template>
+  <div>BLOCK</div>
+  <div>{{ filteredBlocks() }}</div>
+</template>
+<style scoped></style>
