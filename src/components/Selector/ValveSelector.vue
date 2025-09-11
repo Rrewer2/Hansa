d810d3b02b8aa0711e33a405f99d9298d48fb0a7
Rrewer2 <115699656+Rrewer2@users.noreply.github.com>
2025-09-11 12:27:53 +0200

    Update ValveSelector.vue

--- src/components/Selector/ValveSelector.vue
+++ src/components/Selector/ValveSelector.vue
@@ -45,15 +45,9 @@ spoolData.forEach(({ spool }) => set.add(spool));
     <h2 :class="open && 'bgc-g'">
       {{ text("valve") }}<span> {{ order.valve?.title }}</span>
     </h2>
-//    <div class="grid ml-10">
-//      <svg v-for="spool in set" @click="() => (meta.spool = spool)" xmlns="http://www.w3.org/2000/svg"
-//        viewBox="0 0 200 90" class="valve" :class="spool === meta.spool && 'clicked'">
-//        <Valve :x="0" :y="10" :data="{ spool }" :sl="200" />
-//      </svg>
-//    </div>
     <table v-if="filteredValves()[0].length">
       <thead>
-        <td v-for="item in Object.keys(filteredValves()[0][0])">
+        <td v-for="item in Object.keys(filteredValves()[0])">
           <b><i>{{ text(item) }}</i></b>
         </td>
       </thead>
