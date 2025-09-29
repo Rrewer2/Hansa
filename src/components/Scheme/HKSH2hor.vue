a922055f072118f7ed46767bd8d9d94051f597a5
Rrewer2 <rrewer2018@gmail.com>
2025-09-29 20:47:58 +0200

    fixed HKSH2hor

--- src/components/Scheme/HKSH2hor.vue
+++ src/components/Scheme/HKSH2hor.vue
@@ -8,19 +8,22 @@ const k = 0.75;
 <template>
   <HKSHhor :x="x + sl / 4" :y="y - sl / 4" :sl="sl * k" />
   <HKSHhor :x="x + sl / 4" :y="y - (sl * 3) / 4" :sl="sl * k" />
+  <path :d="`M${x + 0.46 * sl} ${y - sl / 20} v${-sl / 8} h${-sl / 8}`" stroke="black" stroke-width="2" fill="none" />
   <path
-    :d="`M${x + 0.46 * sl} ${y - sl / 12} A0.7 1 0 1 0 ${x + 0.46 * sl} ${y - sl / 6} v${-sl / 24} h${-sl / 3} v${(-sl * 5) / 12 + sl / 24} h${sl / 6} v${-sl / 6} v${sl / 6} v${(sl * k) / 5}`"
-    stroke="black" stroke-width="1" fill="none" />
+    :d="`M${x + 0.46 * sl - sl / 8} ${y - sl / 20 - sl / 8} A1 0.5 0 1 1 ${x + 0.46 * sl - sl / 8 - sl / 8} ${y - sl / 20 - sl / 8}`"
+    stroke="black" stroke-width="5" fill="none" />
   <path
-    :d="`M${x + (1 - 0.46) * sl} ${y - sl / 12} A0.7 1 0 1 0 ${x + (1 - 0.46) * sl} ${y - sl / 6} v${-sl / 24} h${sl / 3} h${sl / 20} v${(-sl * 5) / 12 + sl / 24} h${-sl / 3} v${-sl / 6} v${sl / 6} v${(sl * k) / 5}`"
-    stroke="black" stroke-width="1" fill="none" />
+    :d="`M${x + 0.46 * sl - sl / 8 - sl / 8} ${y - sl / 20 - sl / 8} h${-sl / 20} v${-sl * 3 / 8 - sl / 24} h${sl / 8} v${-sl / 6} v${sl / 6} v${sl / 6}`"
+    stroke="black" stroke-width="2" fill="none" />
 
-  <!-- <path
-        :d="`M${x + 0.46 * sl} ${y} A0.7 1 0 1 0 ${x + 0.46 * sl} ${y - sl / 12} v${-sl / 8} h${-sl * 5 / 16} v${-sl / 8} h${-sl / 16} v${-sl / 4} h${sl * 0.5} v${sl / 20}`"
-        stroke="black" stroke-width="2" fill="none" />
-
-    <path :d="`M${x + sl / 3} ${y - sl * 11 / 24} v${-sl / 10} v${sl / 5} v${-sl / 10} h${sl / 2}`" stroke="black"
-        stroke-width="2" fill="none" /> -->
+  <path :d="`M${x + (1 - 0.46) * sl} ${y - sl / 20} v${-sl / 8} h${sl / 8}`" stroke="black" stroke-width="2"
+    fill="none" />
+  <path
+    :d="`M${x + (1 - 0.46) * sl + sl / 8} ${y - sl / 20 - sl / 8} A1 0.5 0 1 0 ${x + (1 - 0.46) * sl + sl / 8 + sl / 8} ${y - sl / 20 - sl / 8}`"
+    stroke="black" stroke-width="5" fill="none" />
+  <path
+    :d="`M${x + (1 - 0.46) * sl + sl / 8 + sl / 8} ${y - sl / 20 - sl / 8} h${sl / 12} v${-sl * 3 / 8 - sl / 24} h${-sl / 6 - sl / 8} v${-sl / 6} v${sl / 6} v${sl / 6}`"
+    stroke="black" stroke-width="2" fill="none" />
 </template>
 
 <style scoped></style>
