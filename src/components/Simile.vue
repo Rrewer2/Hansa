d2ecd2ae1aee1efa46b76ef32766bc8aac9dad99
Rrewer2 <rrewer2018@gmail.com>
2025-10-17 20:50:30 +0200

    changed result in Simile to two textareas

--- src/components/Simile.vue
+++ src/components/Simile.vue
@@ -3,7 +3,7 @@ import { text } from "../services/text";
 
 const { simile } = defineProps(["simile"]);
 const getDiffBetween = (orderStr, modelStr) => {
-  if (!orderStr.trim().length || !modelStr.trim().length) return "";
+  if (!orderStr.trim().length || !modelStr.trim().length) return ["", ""];
   const order = orderStr.split("\n").map((row) => row.split("\t"));
   const model = modelStr.split("\n").map((row) => row.split("\t"));
   const orderObj = {};
@@ -22,76 +22,52 @@ const getDiffBetween = (orderStr, modelStr) => {
     modelObj[trimTitle] ? modelObj[trimTitle].push(N * (KIT || 1)) : (modelObj[trimTitle] = [N * (KIT || 1)]);
   });
   const res = [];
+  const res1 = [];
+  const res2 = [];
   Object.keys(orderObj).forEach((title) => {
     const orderCount = orderObj[title].map(({ N }) => N);
     const orderNumber = orderObj[title].map(({ nr }) => nr);
     if (!modelObj[title]) {
-      res.push([`${orderNumber.join(", ")}`, title, `${orderCount.join(", ")}`].join("\t"));
+      res1.push([`${orderNumber.join(", ")}`, title, `${orderCount.join(", ")}`].join("\t"));
     } else {
       const modelCount = modelObj[title];
       if (orderCount.reduce((a, b) => +a + +b) !== modelCount.reduce((a, b) => +a + +b)) {
-        res.push([`${orderNumber.join(", ")}`, title, `${orderCount.join(", ")}`, `/ ${modelCount.join(", ")}`].join("\t"));
+        res1.push([`${orderNumber.join(", ")}`, title, `${orderCount.join(", ")}`, `/ ${modelCount.join(", ")}`].join("\t"));
       }
     }
   });
-  res.push("	");
+  // res.push("	");
   Object.keys(modelObj).forEach((title) => {
     if (!orderObj[title]) {
-      res.push([title, `${modelObj[title].join(", ")}`].join("\t"));
+      res2.push([title, `${modelObj[title].join(", ")}`].join("\t"));
     }
   });
-  return res.join("\n");
-  // const res = [];
-  // Object.keys(orderObj).forEach((title) => {
-  //   const orderCount = orderObj[title].map(({ N }) => N);
-  //   const orderNumber = orderObj[title].map(({ nr }) => nr);
-  //   if (!modelObj[title]) {
-  //     res.push([orderNumber, title, orderCount]);
-  //   } else {
-  //     const modelCount = modelObj[title];
-  //     if (
-  //       orderCount.reduce((a, b) => +a + +b) !==
-  //       modelCount.reduce((a, b) => +a + +b)
-  //     ) {
-  //       res.push([orderNumber, title, orderCount, modelCount]);
-  //     }
-  //   }
-  // });
-  // const res2 = [];
-  // Object.keys(modelObj).forEach((title) => {
-  //   if (!orderObj[title]) {
-  //     res2.push([title, modelObj[title]]);
-  //   }
-  // });
-  // return [res, res2];
+  res.push(res1.join("\n"));
+  res.push(res2.join("\n"));
+  return res;
 };
 </script>
 
 <template>
   <section>
-    <textarea
-      name="zlec"
-      id="zlec"
-      :placeholder="`${text('inputFromSap')}:
-100 HKHQ012  1`"
-      v-model="simile.zlec"
-    ></textarea>
-    <textarea
-      name="lista"
-      id="lista"
-      :placeholder="`${text('inputFromSolid')}:
-HKHQ012 2`"
-      v-model="simile.lista"
-    ></textarea>
+    <textarea name="zlec" id="zlec" :placeholder="`${text('inputFromSap')}:100 HKHQ012  1`" v-model="simile.zlec"></textarea>
+    <textarea name="lista" id="lista" :placeholder="`${text('inputFromSolid')}: HKHQ012 2`" v-model="simile.lista"></textarea>
   </section>
-  <textarea id="result" :placeholder="text('difference')">{{ getDiffBetween(simile.zlec, simile.lista) }}</textarea>
+  <section>
+    <textarea
+      v-for="(res, i) in getDiffBetween(simile.zlec, simile.lista)"
+      :id="'result' + i"
+      :placeholder="text('difference' + (i + 1))"
+      >{{ res }}</textarea
+    >
+  </section>
 </template>
 
 <style scoped>
 textarea {
   text-align: left;
-  font-size: 1.25rem;
-  height: 50vh;
+  font-size: 1rem;
+  height: 40vh;
   width: 40%;
   margin: 20px 10px;
   padding: 10px;
