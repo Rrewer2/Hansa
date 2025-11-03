103dd32abb57b4567acf799b6de56ed10d6ea4e5
Rrewer2 <rrewer2018@gmail.com>
2025-11-03 21:43:51 +0100

    fixed HKHMP bar position

--- src/components/Scheme/HKHMP.vue
+++ src/components/Scheme/HKHMP.vue
@@ -4,14 +4,7 @@ import Safety from "./Safety.vue";
 import CheckValve from "./CheckValve.vue";
 import Point from "./Point.vue";
 
-const { x, y, r, sl, sh, data } = defineProps([
-  "x",
-  "y",
-  "r",
-  "sl",
-  "sh",
-  "data",
-]);
+const { x, y, r, sl, sh, data } = defineProps(["x", "y", "r", "sl", "sh", "data"]);
 
 const HKHMP = {
   "011": { A: false, B: false, P: true },
@@ -29,8 +22,7 @@ const HKHMP = {
 
     <path :d="`M${x + 0.46 * sl} ${y} v${sh}`" stroke="black" stroke-width="2" fill="none" />
     <path :d="`M${x + (1 - 0.46) * sl} ${y} v${sh}`" stroke="black" stroke-width="2" fill="none" />
-    <text :x="x + sh * 1.9" :y="y + sh * 0.9" font-family="Arial" :font-size="r * 1.7" fill="black"
-      text-anchor="middle">
+    <text :x="x + sl * 0.65" :y="y + sh * 0.9" font-family="Arial" :font-size="r * 1.7" fill="black" text-anchor="middle">
       {{ data.directPressValue }} bar
     </text>
 
@@ -38,8 +30,12 @@ const HKHMP = {
       <Point :x="x" :y="y + sh / 2" />
       <Point :x="x + sl" :y="y + sh / 2" />
       <path :d="`M${x} ${y + sh / 2} h${sl}`" stroke="black" stroke-width="2" fill="none" />
-      <path :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
-        stroke="black" stroke-width="2" fill="none" />
+      <path
+        :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
       <Point :x="x + (1 - 0.46) * sl + sl / 4 + sh / 3" :y="y + sh / 2" />
       <Safety :x="x + (1 - 0.46) * sl + sl / 4" :y="y + sh / 2" :a="sh / 3" direction="right" />
     </template>
@@ -47,10 +43,18 @@ const HKHMP = {
     <template v-if="data.directPress === '012'">
       <Point :x="x + 0.46 * sl" :y="y + sh / 2" />
       <Point :x="x + sl" :y="y + sh / 8" />
-      <path :d="`M${x + 0.46 * sl} ${y + sh / 2} h${-sh} v${-sh / 2 + sh / 8} h${sh + (1 - 0.46) * sl}`" stroke="black"
-        stroke-width="2" fill="none" />
-      <path :d="`M${x + sh / 2 + sh / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${-sh / 3} v${-sh / 2 + sh / 8}`"
-        stroke="black" stroke-width="2" fill="none" />
+      <path
+        :d="`M${x + 0.46 * sl} ${y + sh / 2} h${-sh} v${-sh / 2 + sh / 8} h${sh + (1 - 0.46) * sl}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
+      <path
+        :d="`M${x + sh / 2 + sh / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${-sh / 3} v${-sh / 2 + sh / 8}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
       <Point :x="x + sh / 2 + sh / 4 - sh / 3" :y="y + sh / 2" />
       <Safety :x="x + sh / 2 + sh / 4" :y="y + sh / 2" :a="sh / 3" direction="left" />
     </template>
@@ -59,8 +63,12 @@ const HKHMP = {
       <Point :x="x + 0.46 * sl" :y="y + sh / 2" />
       <Point :x="x + sl" :y="y + sh / 2" />
       <path :d="`M${x + 0.46 * sl} ${y + sh / 2} h${(1 - 0.46) * sl}`" stroke="black" stroke-width="2" fill="none" />
-      <path :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
-        stroke="black" stroke-width="2" fill="none" />
+      <path
+        :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
       <Point :x="x + (1 - 0.46) * sl + sl / 4 + sh / 3" :y="y + sh / 2" />
       <Safety :x="x + (1 - 0.46) * sl + sl / 4" :y="y + sh / 2" :a="sh / 3" direction="right" />
     </template>
@@ -69,8 +77,12 @@ const HKHMP = {
       <Point :x="x + (1 - 0.46) * sl" :y="y + sh / 2" />
       <Point :x="x + sl" :y="y + sh / 2" />
       <path :d="`M${x + (1 - 0.46) * sl} ${y + sh / 2} h${0.46 * sl}`" stroke="black" stroke-width="2" fill="none" />
-      <path :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
-        stroke="black" stroke-width="2" fill="none" />
+      <path
+        :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
       <Point :x="x + (1 - 0.46) * sl + sl / 4 + sh / 3" :y="y + sh / 2" />
       <Safety :x="x + (1 - 0.46) * sl + sl / 4" :y="y + sh / 2" :a="sh / 3" direction="right" />
     </template>
@@ -78,10 +90,18 @@ const HKHMP = {
     <template v-if="data.directPress === '015'">
       <Point :x="x + 0.46 * sl" :y="y + sh / 2" />
       <Point :x="x + (1 - 0.46) * sl" :y="y + sh / 12" />
-      <path :d="`M${x + 0.46 * sl} ${y + sh / 2} h${-sh} v${-sh / 2 + sh / 12} h${sh + (1 - 0.46) * sl - 0.46 * sl}`"
-        stroke="black" stroke-width="2" fill="none" />
-      <path :d="`M${x + sh / 2 + sh / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${-sh / 3} v${-sh / 2 + sh / 8}`"
-        stroke="black" stroke-width="2" fill="none" />
+      <path
+        :d="`M${x + 0.46 * sl} ${y + sh / 2} h${-sh} v${-sh / 2 + sh / 12} h${sh + (1 - 0.46) * sl - 0.46 * sl}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
+      <path
+        :d="`M${x + sh / 2 + sh / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${-sh / 3} v${-sh / 2 + sh / 8}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
       <Point :x="x + sh / 2 + sh / 4 - sh / 3" :y="y + sh / 2" />
       <Safety :x="x + sh / 2 + sh / 4" :y="y + sh / 2" :a="sh / 3" direction="left" />
 
@@ -89,9 +109,16 @@ const HKHMP = {
       <Point :x="x + 0.46 * sl" :y="y + sh / 4" />
       <path
         :d="`M${x + (1 - 0.46) * sl} ${y + sh / 2} h${0.46 * sl - sh / 4 + sh / 16} v${-sh / 4} h${-(1 - 0.46) * sl + sh / 4 - sh / 16}`"
-        stroke="black" stroke-width="2" fill="none" />
-      <path :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
-        stroke="black" stroke-width="2" fill="none" />
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
+      <path
+        :d="`M${x + (1 - 0.46) * sl + sl / 4} ${y + sh / 2 + sh / 4} v${sh / 8} h${sh / 3} v${-sh / 2 + sh / 8}`"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
       <Point :x="x + (1 - 0.46) * sl + sl / 4 + sh / 3" :y="y + sh / 2" />
       <Safety :x="x + (1 - 0.46) * sl + sl / 4" :y="y + sh / 2" :a="sh / 3" direction="right" />
     </template>
