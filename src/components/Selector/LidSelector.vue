9dc63ec1d56067e7c56cf9c120b397243ec4b6e4
Rrewer2 <rrewer2018@gmail.com>
2025-11-11 22:45:47 +0100

    added few more lids

--- src/components/Selector/LidSelector.vue
+++ src/components/Selector/LidSelector.vue
@@ -3,14 +3,16 @@ import { lidData } from "../../services/data";
 import SmthSelector from "./SmthSelector.vue";
 
 const { project, meta, order, open } = defineProps(["project", "meta", "order", "open"]);
-const filteredLid = () =>
-  lidData.filter(
+const filteredLid = () => {
+  const res = lidData.filter(
     (lid) =>
       lid.tank === order["tank"]?.tankData?.Size &&
       (lid.type === order["tank"]?.tankData?.type || lid.type === order["tank"]?.tankData?.[meta?.tank]) &&
       (!order["motor0"]?.motorData?.size || lid.motor === order["motor0"]?.motorData?.size) &&
       (!lid.blockType || order["block0"]?.title?.startsWith(lid.blockType)),
   );
+  return res.length ? res : lidData.filter((lid) => lid.tank === order["tank"]?.tankData?.Size);
+};
 </script>
 
 <template>
