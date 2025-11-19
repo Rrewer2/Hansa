c5482ceacf598e01c86ef3e45a0c4edf0ad29bfa
Rrewer2 <115699656+Rrewer2@users.noreply.github.com>
2025-11-19 12:29:26 +0100

    Create AkuSelector.vue

---
+++ src/components/Selector/AkuSelector.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+import { akuData, akuSafetyData } from "../../services/data";
+import SmthSelector from "./SmthSelector.vue";
+
+const { project, meta, order } = defineProps(["project", "meta", "order", "powerUNIT", "i"]);
+
+const filteredAku = () => {
+  return akuData.filter(({ title, pmax, q, threadA, threadC }) => {
+      return (
+        pmax > project[0]?.unit[0]?.p * 1.3
+        );
+};
+const after = () => {
+  //300 barolitrow max!
+  //order[`xvrBlockT${k}`] = xvrBlockT ? { title: xvrBlockT.title, xvrBlockTData: xvrBlockT } : {};
+};
+</script>
+
+<template>
+  <SmthSelector
+    v-bind="{ meta, order }"
+    Name="aku"
+    :index="ind"
+    :logic="() => filteredAku"
+    :after="() => after()"
+  >
+  </SmthSelector>
+</template>
+<style scoped></style>
