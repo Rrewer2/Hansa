01db938c285434fd2c6b06321a478b14bf2f127f
Rrewer2 <115699656+Rrewer2@users.noreply.github.com>
2026-06-12 14:12:00 +0200

    Update HKSHSelector.vue

--- src/components/Selector/HKSHSelector.vue
+++ src/components/Selector/HKSHSelector.vue
@@ -85,7 +85,7 @@ const getValue = {
         <article class="kok">
           <div class="container">
             <img
-              v-for="(image, i) in { 1: links.HKFL, 2: links.HKCSTSN, 3: links.HKCFL, 4: links.HKCFL, 7: links.HKCSB, W: links.HKCFS }"
+              v-for="(image, i) in { 1: links.HKFL, 2: links.HKCSTSN, 3: links.HKCFL, 4: links.HKCFL, W: links.HKCFS }"
               :src="image"
               :alt="i"
               class="imgLogo rotate270"
@@ -122,7 +122,7 @@ const getValue = {
         <article class="kok">
           <div class="container">
             <img
-              v-for="(image, i) in { 0: links.mountB2, 1: links.HKCOF, 2: links.HKCSTSC, W: links.HKCFF }"
+              v-for="(image, i) in { 0: links.mountB2, 1: links.HKCOF, 2: links.HKCSTSC, 7: links.HKCSB, W: links.HKCFF }"
               :src="image"
               :alt="i"
               class="imgLogo rotate90"
