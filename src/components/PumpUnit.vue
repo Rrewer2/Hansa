33e3d177acf9706c6d6dc230b665f8d5052112bb
Rrewer2 <115699656+Rrewer2@users.noreply.github.com>
2026-07-03 07:24:13 +0200

    Update PumpUnit.vue

--- src/components/PumpUnit.vue
+++ src/components/PumpUnit.vue
@@ -39,7 +39,7 @@ const { id, HKSH, same, startValve, Gerotor, ...rest } = pumpData;
             :id="id + ind"
           />
           <input v-if="ind === 'p'" type="number" min="0" max="700" v-model="pumpData[ind]" :id="id + ind" />
-          <input v-if="ind === 'maxPressure' && meta.pumpType === 'piston'" type="number" min="0" max="700" v-model="pumpData[ind]" :id="id + ind" />
+          <input v-if="ind === 'maxPressure'" type="number" min="0" max="700" v-model="pumpData[ind]" :id="id + ind" />
           <input v-if="ind === 'DBD'" type="number" min="0" max="350" v-model="pumpData[ind]" :id="id + ind" />
           <input v-if="ind === 'aku'" type="number" min="0" max="350" v-model="pumpData[ind]" :id="id + ind" />
           <select v-if="ind === 'DR2type'" v-model="pumpData[ind]" class="w-100" :disabled="same" :id="id + ind">
