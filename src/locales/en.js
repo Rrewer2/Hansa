3b52f3ca0dc416c74afa74dc07f15085331a367f
Rrewer2 <115699656+Rrewer2@users.noreply.github.com>
2026-09-03 08:21:11 +0200

    Update en.js

--- src/locales/en.js
+++ src/locales/en.js
@@ -17,7 +17,7 @@ export default {
   P: ["Power", "kW"],
   Pcalc: ["Power calc.", "kW"],
   I: ["El. current", "A"],
-  VFU: ["VFU", "cm³/rev"],
+  VFU: ["VFU", "cm³/rev", "gpm"],
   k: ["Ratio"],
   pipeP: ["Pipe P"],
   pipeT: ["Pipe T"],
